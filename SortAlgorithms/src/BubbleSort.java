d556767388713444fd672103c9d49a1944655e6c
Apple <apple@Apples-MacBook-Air.local>
2021-06-29 00:33:52 -0600

    done with bubblesort and selectionsort

--- SortAlgorithms/src/BubbleSort.java
+++ SortAlgorithms/src/BubbleSort.java
@@ -1,4 +1,26 @@
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class BubbleSort {
+	public static void swap(int[] array,int i,int j) {
+		
+		int temp = array[i];
+		array[i] = array[j];
+		array[j]  = temp;
+	}
+		public static void main(String[] args) {
+			int[] intArray = {23,451,15,1345,67,-43,-1,0};
+			for(int lastUnsortedIndex = intArray.length-1;lastUnsortedIndex>0;lastUnsortedIndex--) {
+				for(int b=0;b<lastUnsortedIndex;b++) {
+					if(intArray[b]>intArray[b+1]) {
+						swap(intArray,b,b+1);
+					}
+				}
+				
 
+			}
+			System.out.println("Bubble sorted array: "+Arrays.toString(intArray));
+		}
+		
 }
---
+++ SortAlgorithms/src/SelectionSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+ public static void swap(int[] arr,int i,int j) {
+	 int temp = arr[i];
+	 arr[i]=arr[j];
+	 arr[j] = temp;
+ }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] intArray = {19,44,38,5,47,15};
+		for(int i= 0;i<intArray.length;i++) {
+			int lowest = i;
+			for(int j=i+1;j<intArray.length;j++) {
+				if(intArray[j]<intArray[lowest]) {
+					lowest=j;
+				}
+			}
+			if(i!=lowest) {
+				swap(intArray,i,lowest);
+			}
+			System.out.println("Selection sorted array: "+Arrays.toString(intArray));
+
+		}
+	}
+
+}
